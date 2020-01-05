988310df975242633d9d6ba6669e521e6c5bba34
Wouter Hisschemöller <email@hisschemoller.com>
2020-01-05 21:44:14 +0100

    OSC and libusb investigation.

--- README.md
+++ README.md
@@ -10,4 +10,33 @@ The VCV Rack modules are part of the plugin Arduino Modules located at `/vcvrack
 
 ### Module
 
-The first module is called 'Arduino Test'.
+The first module is called 'Arduino MIDI'.
+
+## Notes
+
+### libusb
+
+`libusb` is a library for communication with USB devices: https://libusb.info/<br />
+On Mac, use Homebrew to install libusb:
+
+```bash
+brew install libusb
+```
+
+On Mac libusb will be installed at:
+
+```
+/usr/local/Cellar/libusb/<version nr, example: 1.0.21>/lib/libusb-1.0.a
+```
+
+### Arduino Leonardo Mac OS Mojave
+
+Arduino Leonardo USB port doesn't appear.<br>
+Possible fix: https://arduino.stackexchange.com/questions/57126/connect-to-ch340-on-macos-mojave
+
+```bash
+sudo rm -rf /Library/Extensions/usbserial.kext
+sudo rm -rf /System/Library/Extensions/usb.kext
+```
+
+Then reboot.
---
+++ arduino/BasicSendReceiveOSC/BasicSendReceiveOSC.ino
@@ -0,0 +1,73 @@
+#include <SLIPEncodedSerial.h>
+#include <OSCBundle.h>
+#if 0
+#include <OSCData.h>
+#include <OSCBoards.h>
+#include <OSCTiming.h>
+#include <OSCMessage.h>
+#include <OSCMatch.h>
+#include <SLIPEncodedUSBSerial.h>
+#endif
+
+/**
+ * Using OSC library: https://github.com/CNMAT/OSC
+ * Open Sound Control: http://opensoundcontrol.org
+ * Clearer example: https://github.com/CNMAT/OSC/issues/29
+ */
+
+SLIPEncodedSerial SLIPSerial(Serial);
+int testValue = 1337;
+
+// basic timer
+unsigned long timer = 0;             // will store last time  updated
+unsigned long timerInterval = 1000;  // interval at which to time (milliseconds)
+
+void setup() {
+  SLIPSerial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // check for new messages as fast as possible
+  receiveOSC();
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - timer > timerInterval) {
+    timer = currentMillis;
+    sendOSC();
+  }
+}
+
+void receiveOSC() {
+  OSCMessage messageIN;
+  int size;
+
+  // receive a bundle
+  if (SLIPSerial.available()) {
+    while (!SLIPSerial.endofPacket()) {
+      size = SLIPSerial.available();
+      if (size > 0) {
+        while (size--) {
+          messageIN.fill(SLIPSerial.read());
+        }
+        if (!messageIN.hasError()) {
+          messageIN.dispatch("/test/in", action);
+        }
+      }
+    }
+  }
+}
+
+void action(OSCMessage &msgIn) {
+  testValue = msgIn.getInt(0);
+  digitalWrite(LED_BUILTIN, testValue > 0 ? HIGH : LOW); 
+}
+
+void sendOSC() {
+  OSCMessage msg("/test/out");
+  msg.add(testValue);
+  SLIPSerial.beginPacket();
+  msg.send(SLIPSerial); // send the bytes to the SLIP stream
+  SLIPSerial.endPacket(); // mark the end of the OSC Packet
+  msg.empty(); // free space occupied by message
+}
