c2fea02d20645de378a916c9d71aea1059814704
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2020-01-22 14:38:51 +0100

    Prototype1 sends MIDI for buttons, potmeter and encoder.

--- arduino/Prototype1/Prototype1.ino
+++ arduino/Prototype1/Prototype1.ino
@@ -1,3 +1,9 @@
+#include <MIDIUSB_Defs.h>
+#include <MIDIUSB.h>
+
+const uint8_t MIDI_CHANNEL = 0;
+const uint8_t MIDI_CC_BUTTONS = 14;
+const uint8_t MIDI_CC_POTMETER = 15;
 const uint8_t NUM_LEDS = 8;
 const uint8_t PIN_PUSH_BTN = 8;
 const uint8_t PIN_ENCODER_BTN = 9;
@@ -9,6 +15,7 @@ int encoderBtnState = 0;
 int encoderAState;
 int encoderCounter = 0;
 float potMeterValue = 0;
+int potmeterCCValue = 0;
 int potmeterLed = 0;
 
 void setup() {
@@ -24,30 +31,51 @@ void setup() {
 }
 
 void loop() {
+  // send pushbutton as MIDI CC
   if (pushBtnState != digitalRead(PIN_PUSH_BTN)) {
     pushBtnState = digitalRead(PIN_PUSH_BTN);
-    updateLEDs(0, pushBtnState);
+    int pushbuttonCCValue = pushBtnState ? 9 : 8;
+    sendMIDIMessage(0x0b, 0xb0 | MIDI_CHANNEL, MIDI_CC_BUTTONS, pushbuttonCCValue);
+    // updateLEDs(0, pushBtnState);
   }
+
+  // send encoder button as MIDI CC
   if (encoderBtnState != digitalRead(PIN_ENCODER_BTN)) {
     encoderBtnState = digitalRead(PIN_ENCODER_BTN);
-    updateLEDs(1, encoderBtnState);
+    int encoderbuttonCCValue = encoderBtnState ? 11 : 10;
+    sendMIDIMessage(0x0b, 0xb0 | MIDI_CHANNEL, MIDI_CC_BUTTONS, encoderbuttonCCValue);
+    // updateLEDs(1, encoderBtnState);
   }
+
+  // send potmeter value as MIDI CC
   if (potMeterValue != analogRead(PIN_POTMETER)) {
     potMeterValue = analogRead(PIN_POTMETER);
-    if (potmeterLed != (int)(potMeterValue / 128)) {
-      potmeterLed = potMeterValue / 128;
-      updateLEDs(potmeterLed, 1);
+    if (potmeterCCValue != (int)(potMeterValue / 8)) {
+      potmeterCCValue = potMeterValue / 8;
+      sendMIDIMessage(0x0b, 0xb0 | MIDI_CHANNEL, MIDI_CC_POTMETER, potmeterCCValue);
     }
+    // if (potmeterLed != (int)(potMeterValue / 128)) {
+    //   potmeterLed = potMeterValue / 128;
+    //   updateLEDs(potmeterLed, 1);
+    // }
   }
   if (encoderAState != digitalRead(PIN_ENCODER_A)) {
     encoderAState = digitalRead(PIN_ENCODER_A);
-    if (digitalRead(PIN_ENCODER_B) != encoderAState) {
-      encoderCounter--;
-    } else {
-      encoderCounter++;
-    }
-    updateLEDs(encoderCounter % 8, 1);
+    int encoderOutputCCValue = digitalRead(PIN_ENCODER_B) != encoderAState ? 14 : 13;
+    sendMIDIMessage(0x0b, 0xb0 | MIDI_CHANNEL, MIDI_CC_BUTTONS, encoderOutputCCValue);
+    // if (digitalRead(PIN_ENCODER_B) != encoderAState) {
+    //   encoderCounter--;
+    // } else {
+    //   encoderCounter++;
+    // }
+    // updateLEDs(encoderCounter % 8, 1);
   }
+}
+
+void sendMIDIMessage(uint8_t header, uint8_t byte0, uint8_t byte1, uint8_t byte2) {
+  midiEventPacket_t midiEvent = {header, byte0, byte1, byte2};
+  MidiUSB.sendMIDI(midiEvent);
+  MidiUSB.flush();
 }
 
 void updateLEDs(int index, int isOn) {
