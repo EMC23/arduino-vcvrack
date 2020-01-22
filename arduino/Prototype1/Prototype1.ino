0ae61a2cec8a6a1c0446099a6062db1c18360bb4
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2020-01-22 16:33:28 +0100

    Prototype1 receives MIDI for LEDs.

--- arduino/Prototype1/Prototype1.ino
+++ arduino/Prototype1/Prototype1.ino
@@ -2,8 +2,10 @@
 #include <MIDIUSB.h>
 
 const uint8_t MIDI_CHANNEL = 0;
-const uint8_t MIDI_CC_BUTTONS = 14;
+const uint8_t MIDI_CC_BINARY = 14;
 const uint8_t MIDI_CC_POTMETER = 15;
+const uint8_t MIDI_CC_TYPE = 0xb0;
+const uint8_t MIDI_MSG_HEADER = 0x0b
 const uint8_t NUM_LEDS = 8;
 const uint8_t PIN_PUSH_BTN = 8;
 const uint8_t PIN_ENCODER_BTN = 9;
@@ -31,11 +33,42 @@ void setup() {
 }
 
 void loop() {
+  // receive MIDI
+  midiEventPacket_t rx;
+  do {
+    rx = MidiUSB.read();
+    if (rx.header != 0) {
+      
+      if (rx.header == 9) {
+        digitalWrite(LED_BUILTIN, HIGH);
+      } else if (rx.header == 8) {
+        digitalWrite(LED_BUILTIN, LOW);
+      }
+      
+      Serial.print("Received: ");
+      Serial.print(rx.header, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte1, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte2, HEX);
+      Serial.print("-");
+      Serial.println(rx.byte3, HEX);
+
+			// type = rx.byte1 & 0xf0, channel = rx.byte1 & 0xf) + 1
+      if (rx.byte1 & 0xf0 == MIDI_CC_TYPE && rx.byte1 & 0xf) == MIDI_CHANNEL && rx.byte2 == MIDI_CC_BINARY) {
+        if (rx.byte3 >= 16 && rx.byte3 < 31) {
+          int ledIndex = (int)(rx.byte3 - 16) / 2;
+          digitalWrite(ledIndex, rx.byte3 % 2 == 1);
+        }
+      }
+    }
+  } while (rx.header != 0);
+
   // send pushbutton as MIDI CC
   if (pushBtnState != digitalRead(PIN_PUSH_BTN)) {
     pushBtnState = digitalRead(PIN_PUSH_BTN);
     int pushbuttonCCValue = pushBtnState ? 9 : 8;
-    sendMIDIMessage(0x0b, 0xb0 | MIDI_CHANNEL, MIDI_CC_BUTTONS, pushbuttonCCValue);
+    sendMIDIMessage(0x0b, MIDI_CC_TYPE | MIDI_CHANNEL, MIDI_CC_BINARY, pushbuttonCCValue);
     // updateLEDs(0, pushBtnState);
   }
 
@@ -43,7 +76,7 @@ void loop() {
   if (encoderBtnState != digitalRead(PIN_ENCODER_BTN)) {
     encoderBtnState = digitalRead(PIN_ENCODER_BTN);
     int encoderbuttonCCValue = encoderBtnState ? 11 : 10;
-    sendMIDIMessage(0x0b, 0xb0 | MIDI_CHANNEL, MIDI_CC_BUTTONS, encoderbuttonCCValue);
+    sendMIDIMessage(0x0b, MIDI_CC_TYPE | MIDI_CHANNEL, MIDI_CC_BINARY, encoderbuttonCCValue);
     // updateLEDs(1, encoderBtnState);
   }
 
@@ -52,7 +85,7 @@ void loop() {
     potMeterValue = analogRead(PIN_POTMETER);
     if (potmeterCCValue != (int)(potMeterValue / 8)) {
       potmeterCCValue = potMeterValue / 8;
-      sendMIDIMessage(0x0b, 0xb0 | MIDI_CHANNEL, MIDI_CC_POTMETER, potmeterCCValue);
+      sendMIDIMessage(0x0b, MIDI_CC_TYPE | MIDI_CHANNEL, MIDI_CC_POTMETER, potmeterCCValue);
     }
     // if (potmeterLed != (int)(potMeterValue / 128)) {
     //   potmeterLed = potMeterValue / 128;
@@ -62,7 +95,7 @@ void loop() {
   if (encoderAState != digitalRead(PIN_ENCODER_A)) {
     encoderAState = digitalRead(PIN_ENCODER_A);
     int encoderOutputCCValue = digitalRead(PIN_ENCODER_B) != encoderAState ? 14 : 13;
-    sendMIDIMessage(0x0b, 0xb0 | MIDI_CHANNEL, MIDI_CC_BUTTONS, encoderOutputCCValue);
+    sendMIDIMessage(0x0b, MIDI_CC_TYPE | MIDI_CHANNEL, MIDI_CC_BINARY, encoderOutputCCValue);
     // if (digitalRead(PIN_ENCODER_B) != encoderAState) {
     //   encoderCounter--;
     // } else {
