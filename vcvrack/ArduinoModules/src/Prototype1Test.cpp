c44a17f700e92f6ea85bc50bb65654890ccaaaa7
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2020-01-23 12:45:04 +0100

    Setup Prototype1Test module.

---
+++ vcvrack/ArduinoModules/src/Prototype1Test.cpp
@@ -0,0 +1,105 @@
+#include "plugin.hpp"
+
+/**
+ * 
+ */
+
+struct MidiOutput : dsp::MidiGenerator<PORT_MAX_CHANNELS>, midi::Output {
+
+	void onMessage(midi::Message message) override {
+		midi::Output::sendMessage(message);
+	}
+
+	void reset() {
+		Output::reset();
+		MidiGenerator::reset();
+	}
+};
+
+struct Prototype1Test : Module {
+
+	enum LightIds {
+		NOTE_IN_LIGHT,
+		NUM_LIGHTS
+	};
+
+  midi::InputQueue midiInput;
+  MidiOutput midiOutput;
+
+  Prototype1Test() {
+    config(0, 0, 0, NUM_LIGHTS);
+		onReset();
+  }
+
+	void onReset() override {
+		midiInput.reset();
+		midiOutput.reset();
+	}
+
+  void process(const ProcessArgs& args) override {
+		midi::Message msg;
+		while (midiInput.shift(&msg)) {
+			processMessage(msg);
+		}
+	}
+
+	void processMessage(midi::Message msg) {
+		switch (msg.getStatus()) {
+			// note off
+			case 0x8: {
+				releaseNote(msg.getNote());
+			} break;
+			// note on
+			case 0x9: {
+				if (msg.getValue() > 0) {
+					pressNote(msg.getNote(), msg.getValue());
+				}
+				else {
+					// Many stupid keyboards send a "note on" command with 0 velocity to mean "note release"
+					releaseNote(msg.getNote());
+				}
+			} break;
+			default: break;
+		}
+	}
+
+	void pressNote(uint8_t note, uint8_t vel) {
+		lights[NOTE_IN_LIGHT].setBrightness(1.f);
+
+		midiOutput.setVelocity(0, 1);
+		midiOutput.setNoteGate(60, false, 1);
+	}
+
+	void releaseNote(uint8_t note) {
+		lights[NOTE_IN_LIGHT].setBrightness(0.f);
+
+		midiOutput.setVelocity(100, 1);
+		midiOutput.setNoteGate(60, true, 1);
+	}
+};
+
+struct Prototype1TestWidget : ModuleWidget {
+  Prototype1TestWidget(Prototype1Test* module) {
+    setModule(module);
+    setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Prototype1Test.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+    MidiWidget* midiInputWidget = createWidget<MidiWidget>(mm2px(Vec(3.41891, 14.8373)));
+		midiInputWidget->box.size = mm2px(Vec(33.840, 28));
+		midiInputWidget->setMidiPort(module ? &module->midiInput : NULL);
+		addChild(midiInputWidget);
+
+		MidiWidget* midiOutputWidget = createWidget<MidiWidget>(mm2px(Vec(3.41891, 44.8373)));
+		midiOutputWidget->box.size = mm2px(Vec(33.840, 28));
+		midiOutputWidget->setMidiPort(module ? &module->midiOutput : NULL);
+		addChild(midiOutputWidget);
+
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(10, 80)), module, Prototype1Test::NOTE_IN_LIGHT));
+  }
+};
+
+Model* modelPrototype1Test = createModel<Prototype1Test, Prototype1TestWidget>("Prototype1Test");
--- vcvrack/ArduinoModules/src/plugin.hpp
+++ vcvrack/ArduinoModules/src/plugin.hpp
@@ -9,3 +9,4 @@ extern Plugin* pluginInstance;
 
 // Declare each Model, defined in each module source file
 extern Model* modelArduinoMIDI;
+extern Model* modelPrototype1Test;
