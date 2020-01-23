1eb2d437fd997530a207bfd3f9934b74a5d2a93b
Wouter Hisschemöller <email@hisschemoller.com>
2020-01-23 21:34:05 +0100

    Prototype1Test added LEDs and JSON storage.

--- vcvrack/ArduinoModules/src/Prototype1Test.cpp
+++ vcvrack/ArduinoModules/src/Prototype1Test.cpp
@@ -3,7 +3,6 @@
 /**
  * 
  */
-
 struct MidiOutput : dsp::MidiGenerator<PORT_MAX_CHANNELS>, midi::Output {
 
 	void onMessage(midi::Message message) override {
@@ -16,10 +15,20 @@ struct MidiOutput : dsp::MidiGenerator<PORT_MAX_CHANNELS>, midi::Output {
 	}
 };
 
+/**
+ * 
+ */
 struct Prototype1Test : Module {
 
 	enum LightIds {
-		NOTE_IN_LIGHT,
+		LED_0,
+		LED_1,
+		LED_2,
+		LED_3,
+		LED_4,
+		LED_5,
+		LED_6,
+		LED_7,
 		NUM_LIGHTS
 	};
 
@@ -64,17 +73,41 @@ struct Prototype1Test : Module {
 	}
 
 	void pressNote(uint8_t note, uint8_t vel) {
-		lights[NOTE_IN_LIGHT].setBrightness(1.f);
+		lights[LED_0].setBrightness(1.f);
 
 		midiOutput.setVelocity(0, 1);
 		midiOutput.setNoteGate(60, false, 1);
 	}
 
 	void releaseNote(uint8_t note) {
-		lights[NOTE_IN_LIGHT].setBrightness(0.f);
+		lights[LED_0].setBrightness(0.f);
 
 		midiOutput.setVelocity(100, 1);
 		midiOutput.setNoteGate(60, true, 1);
+	}
+
+	/**
+	 * Save module state to JSON.
+	 */
+	json_t* dataToJson() override {
+		json_t* rootJSON = json_object();
+		json_object_set_new(rootJSON, "midiInput", midiInput.toJson());
+		json_object_set_new(rootJSON, "midiOutput", midiOutput.toJson());
+		return rootJSON;
+	}
+
+	/**
+	 * Load module state from JSON.
+	 */
+	void dataFromJson(json_t* rootJSON) override {
+		json_t* midiInputJSON = json_object_get(rootJSON, "midiInput");
+		if (midiInputJSON) {
+			midiInput.fromJson(midiInputJSON);
+		}
+		json_t* midiOutputJSON = json_object_get(rootJSON, "midiOutput");
+		if (midiOutputJSON) {
+			midiOutput.fromJson(midiOutputJSON);
+		}
 	}
 };
 
@@ -88,17 +121,25 @@ struct Prototype1TestWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-    MidiWidget* midiInputWidget = createWidget<MidiWidget>(mm2px(Vec(3.41891, 14.8373)));
+    MidiWidget* midiInputWidget = createWidget<MidiWidget>(mm2px(Vec(3.41891, 16)));
 		midiInputWidget->box.size = mm2px(Vec(33.840, 28));
 		midiInputWidget->setMidiPort(module ? &module->midiInput : NULL);
 		addChild(midiInputWidget);
 
-		MidiWidget* midiOutputWidget = createWidget<MidiWidget>(mm2px(Vec(3.41891, 44.8373)));
+		MidiWidget* midiOutputWidget = createWidget<MidiWidget>(mm2px(Vec(3.41891, 46)));
 		midiOutputWidget->box.size = mm2px(Vec(33.840, 28));
 		midiOutputWidget->setMidiPort(module ? &module->midiOutput : NULL);
 		addChild(midiOutputWidget);
 
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(10, 80)), module, Prototype1Test::NOTE_IN_LIGHT));
+		float center = 20.25;
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(center - 9, 80)), module, Prototype1Test::LED_0));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(center - 3, 80)), module, Prototype1Test::LED_1));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(center + 3, 80)), module, Prototype1Test::LED_2));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(center + 9, 80)), module, Prototype1Test::LED_3));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(center - 9, 86)), module, Prototype1Test::LED_4));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(center - 3, 86)), module, Prototype1Test::LED_5));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(center + 3, 86)), module, Prototype1Test::LED_6));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(center + 9, 86)), module, Prototype1Test::LED_7));
   }
 };
 
