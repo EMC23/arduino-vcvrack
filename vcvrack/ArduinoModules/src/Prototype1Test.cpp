ebfe157da86a29877ba25df22b3c9124bc295eda
Wouter Hisschemöller <email@hisschemoller.com>
2020-01-30 20:33:10 +0100

    Potmeter and pushbutton value sent to module outputs.

--- vcvrack/ArduinoModules/src/Prototype1Test.cpp
+++ vcvrack/ArduinoModules/src/Prototype1Test.cpp
@@ -57,6 +57,9 @@ struct Prototype1Test : Module {
 		NUM_INPUTS
 	};
 	enum OutputIds {
+		OUTPUT_POTMETER,
+		OUTPUT_POTMETER_INVERSE,
+		OUTPUT_PUSHBUTTON,
 		NUM_OUTPUTS
 	};
 	enum LightIds {
@@ -132,11 +135,13 @@ struct Prototype1Test : Module {
 	void processMIDIInputCC(uint8_t* bytes, uint8_t size) {
 		if (size == 3) {
 			switch (bytes[1]) {
-				case MIDI_CC_BINARY:
+				case MIDI_CC_BINARY: {
 					switch (bytes[2]) {
 						case PUSH_BUTTON_OFF:
+							outputs[OUTPUT_PUSHBUTTON].value = 0;
 							break;
 						case PUSH_BUTTON_ON:
+							outputs[OUTPUT_PUSHBUTTON].value = 5;
 							break;
 						case ENCODER_BUTTON_OFF:
 							break;
@@ -150,9 +155,13 @@ struct Prototype1Test : Module {
 							break;
 					}
 					break;
-				case MIDI_CC_POTMETER:
-					
+				}
+				case MIDI_CC_POTMETER: {
+					double potmeterValue = (bytes[2] / 127.f) * 5.f;
+					outputs[OUTPUT_POTMETER].value = potmeterValue;
+					outputs[OUTPUT_POTMETER_INVERSE].value = 5 - potmeterValue;
 					break;
+				}
 				default:
 					break;
 			}
@@ -222,6 +231,10 @@ struct Prototype1TestWidget : ModuleWidget {
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(center - 4.5, 105)), module, Prototype1Test::INPUT_5));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(center + 4.5, 105)), module, Prototype1Test::INPUT_6));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(center + 13.5, 105)), module, Prototype1Test::INPUT_7));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(center - 13.5, 115)), module, Prototype1Test::OUTPUT_POTMETER));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(center - 4.5, 115)), module, Prototype1Test::OUTPUT_POTMETER_INVERSE));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(center + 4.5, 115)), module, Prototype1Test::OUTPUT_PUSHBUTTON));
   }
 };
 
