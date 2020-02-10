299fcd7dc1d415699f2421f08dc53431def2ffe0
Wouter Hisschemöller <email@hisschemoller.com>
2020-02-10 21:25:17 +0100

    Bug fix in Prototype1B repeating note sketch.

--- arduino/Prototype1B/Prototype1B.ino
+++ arduino/Prototype1B/Prototype1B.ino
@@ -16,25 +16,23 @@
 bool isNoteOn = false;
 bool isClockOn = false;
 
-void clockOut16PPQN() {
+void clockOut16PPQN(uint32_t * tick) {
   if (isNoteOn) {
     sendMIDIMessage(0x09, NOTE_ON | MIDI_CHANNEL, 60, 100);
-    digitalWrite(2, 1);
+    digitalWrite(2, true);
   } else {
-    sendMIDIMessage(0x08, NOTE_OFF | MIDI_CHANNEL, 60, 100);
-    digitalWrite(2, 0);
+    sendMIDIMessage(0x08, NOTE_OFF | MIDI_CHANNEL, 60, 0);
+    digitalWrite(2, false);
   }
   isNoteOn = !isNoteOn;
 }
 
 // The callback function wich will be called by Clock each Pulse of 96PPQN clock resolution.
 void clockOut96PPQN(uint32_t * tick) {
-  // Send MIDI_CLOCK to external gears
-  // Serial.write(MIDI_CLOCK);
   if (isClockOn) {
-    digitalWrite(1, 1);
+    digitalWrite(1, true);
   } else {
-    digitalWrite(1, 0);
+    digitalWrite(1, false);
   }
   isClockOn = !isClockOn;
 }
@@ -55,12 +53,6 @@ void sendMIDIMessage(uint8_t header, uint8_t byte0, uint8_t byte1, uint8_t byte2
   MidiUSB.flush();
 }
 
-// void updateLEDs(int index, int isOn) {
-//   for (int i = 0; i < NUM_LEDS; i++) {
-//     digitalWrite(i, i == index ? isOn : 0);
-//   }
-// }
-
 void setup() {
   #ifdef MIDI_MODE
     // the default MIDI serial speed communication at 31250 bits per second
@@ -72,14 +64,14 @@ void setup() {
   #endif
 
   uClock.init();
-  uClock.setClock16PPQNOutput(clockOut16PPQN);  
+  uClock.setClock16PPQNOutput(clockOut16PPQN);
   // Set the callback function for the clock output to send MIDI Sync message.
   uClock.setClock96PPQNOutput(clockOut96PPQN);
   // Set the callback function for MIDI Start and Stop messages.
   uClock.setOnClockStartOutput(onClockStart);  
   uClock.setOnClockStopOutput(onClockStop);
   // Set the clock BPM to 126 BPM
-  uClock.setTempo(110);
+  uClock.setTempo(100);
 
   // Starts the clock, tick-tac-tick-tac...
   uClock.start();
